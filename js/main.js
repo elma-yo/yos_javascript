40766d5631bcbaf71ecf8ad3f1faf85760df6df4
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 12:10:23 +0900

    querySelectorAllを通してpの2番目の内容を変更

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@
 {
     function update(){
         // document.getElementById('target').textContent = 'Change!';
-        document.querySelector('p').textContent = 'Change!';
+        // document.querySelector('p').textContent = 'Change!';
+        document.querySelectorAll('p')[1].textContent = 'Change!';
     }
 
     setTimeout(update, 1000);
