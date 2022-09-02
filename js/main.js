5bd71a21c7898bc51284ed6ac612b10532780be3
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 16:29:48 +0900

    datasetを通してカスタムデータ属性を扱う

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
     document.querySelector('button').addEventListener('click', () => {
         const targetNode = document.getElementById('target');
 
-        targetNode.textContent = 'Dotinstall';
+        // targetNode.textContent = 'Dotinstall';
+        targetNode.textContent = targetNode.dataset.translation;
     });
 }
