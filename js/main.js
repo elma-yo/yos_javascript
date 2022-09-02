9cb7e56169cb1373b942cba00b1d244e95902e1b
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 13:02:06 +0900

    Runボタンをクリックすると、my-colorとのstyleを反応

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
     document.querySelector('button').addEventListener('click', () => {
         const targetNode = document.getElementById('target');
 
-        targetNode.textContent = 'Change!';
-        targetNode.title = 'This is title!';
-        targetNode.style.color = 'white';
-        targetNode.style.backgroundColor = 'skyblue';
+        targetNode.className = 'my-color'
     });
 }
