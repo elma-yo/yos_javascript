6e6fa5ae84a44c73bce62f8ad79ece395637dc53
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 21:23:35 +0900

    15.ラジオボタンを操作するコード

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@
 
 {
     document.querySelector('button').addEventListener('click', () => {
-        const li =document.createElement('li');
-        const color = document.querySelector('select');
-        li.textContent = `${color.value} - ${color.selectedIndex}`;
+        const colors = document.querySelectorAll('input');
+        let selectedColor;
+
+        colors.forEach(color => {
+            if (color.checked === true) {
+                selectedColor = color.value;
+            }
+        });
+
+        const li = document.createElement('li');
+        li.textContent = selectedColor;
         document.querySelector('ul').appendChild(li);
     });
 }
