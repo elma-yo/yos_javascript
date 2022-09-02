bcae1854a0b874912451e3fcfb27a05577e4bb88
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 12:13:00 +0900

    `${index}番目のpです！`と表示

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@
     function update(){
         // document.getElementById('target').textContent = 'Change!';
         // document.querySelector('p').textContent = 'Change!';
-        document.querySelectorAll('p')[1].textContent = 'Change!';
+        // document.querySelectorAll('p')[1].textContent = 'Change!';
+        document.querySelectorAll('p').forEach((p, index) => {
+            p.textContent = `${index}番目のpです！`;
+        });
     }
 
     setTimeout(update, 1000);
