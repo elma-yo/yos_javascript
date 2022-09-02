025a2fb6d9a2b512d8f4cee18fbc3feff29648d1
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 12:16:01 +0900

    Runとのボタンを追加

--- js/main.js
+++ js/main.js
@@ -2,12 +2,7 @@
 
 {
     function update(){
-        // document.getElementById('target').textContent = 'Change!';
-        // document.querySelector('p').textContent = 'Change!';
-        // document.querySelectorAll('p')[1].textContent = 'Change!';
-        document.querySelectorAll('p').forEach((p, index) => {
-            p.textContent = `${index}番目のpです！`;
-        });
+        document.getElementById('target').textContent = 'Change!';
     }
 
     setTimeout(update, 1000);
