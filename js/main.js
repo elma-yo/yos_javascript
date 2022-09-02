9e9d61266c2413651be11b2829f0343c601be93d
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 16:27:13 +0900

    Runボタンをクリックすると、h1の内容は英語になる

--- js/main.js
+++ js/main.js
@@ -4,13 +4,6 @@
     document.querySelector('button').addEventListener('click', () => {
         const targetNode = document.getElementById('target');
 
-        // targetNode.className = 'my-color my-border'
-        // targetNode.classList.add('my-color');
-        // if (targetNode.classList.contains('my-color') === true) {
-        //     targetNode.classList.remove('my-color');
-        // } else {
-        //     targetNode.classList.add('my-color');
-        // }
-        targetNode.classList.toggle('my-color');
+        targetNode.textContent = 'Dotinstall';
     });
 }
