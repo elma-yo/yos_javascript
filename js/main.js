6f4cb7f1a2098add425b2c2d45769f435150fc4f
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 16:24:54 +0900

    toggleを通してmy-colorを追加/削除

--- js/main.js
+++ js/main.js
@@ -6,10 +6,11 @@
 
         // targetNode.className = 'my-color my-border'
         // targetNode.classList.add('my-color');
-        if (targetNode.classList.contains('my-color') === true) {
-            targetNode.classList.remove('my-color');
-        } else {
-            targetNode.classList.add('my-color');
-        }
+        // if (targetNode.classList.contains('my-color') === true) {
+        //     targetNode.classList.remove('my-color');
+        // } else {
+        //     targetNode.classList.add('my-color');
+        // }
+        targetNode.classList.toggle('my-color');
     });
 }
