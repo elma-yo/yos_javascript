7d7d9c049c5fcc2756a6409372726528086894ec
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 21:46:12 +0900

    mousemoveの座標のコードを追加

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@
         console.log('Double Clicked!');
     });
 
-    document.addEventListener('mousemove', () => {
-        console.log('moved');
+    // document.addEventListener('mousemove', () => {
+    //     console.log('moved');
+    document.addEventListener('mousemove', e => {
+        console.log(e.clientX, e.clientY);
     });
 }
