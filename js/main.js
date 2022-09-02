9fc3280399de4fb19f1feafdefdfeaaa5eff3be6
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 12:08:38 +0900

    querySelectorを通してpの内容を変更

--- js/main.js
+++ js/main.js
@@ -2,9 +2,8 @@
 
 {
     function update(){
-        // document.querySelector('h1').textContent = 'Change!';
-        // document.querySelector('#target').textContent = 'Change!';
-        document.getElementById('target').textContent = 'Change!';
+        // document.getElementById('target').textContent = 'Change!';
+        document.querySelector('p').textContent = 'Change!';
     }
 
     setTimeout(update, 1000);
