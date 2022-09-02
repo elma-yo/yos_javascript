8048859befa45cde2bb43363b234c697665f689c
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 12:06:08 +0900

    h1のidをを追加。getElementByIdを通してh1の内容を変更

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@
 
 {
     function update(){
-        document.querySelector('h1').textContent = 'Change!';
+        // document.querySelector('h1').textContent = 'Change!';
+        // document.querySelector('#target').textContent = 'Change!';
+        document.getElementById('target').textContent = 'Change!';
     }
 
     setTimeout(update, 1000);
