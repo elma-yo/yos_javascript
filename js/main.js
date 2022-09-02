db65c38cb5ba0096210cccbb51e6bed92a07f39f
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 16:52:30 +0900

    removeChildとの方法で、item1を削除

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@
     document.querySelector('button').addEventListener('click', () => {
         const item1 = document.querySelectorAll('li')[1];
 
-        item1.remove();
+        // item1.remove();
+        // 親Node.removeChild(削除するNode)
+        document.querySelector('ul').removeChild(item1);
     });
 }
