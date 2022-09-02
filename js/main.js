eaf7aa23d7486de96990c36340c2424c117a510c
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 16:17:04 +0900

    styleの反応でmy-borderを追加

--- js/main.js
+++ js/main.js
@@ -4,6 +4,6 @@
     document.querySelector('button').addEventListener('click', () => {
         const targetNode = document.getElementById('target');
 
-        targetNode.className = 'my-color'
+        targetNode.className = 'my-color my-border'
     });
 }
