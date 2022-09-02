14a944fd754efc1aca096b760f0ac161c8503816
~elma-yo~ <~elmaspy@gmail.com~>
2022-09-02 20:27:51 +0900

    inputで入力した値を取得

--- js/main.js
+++ js/main.js
@@ -2,10 +2,9 @@
 
 {
     document.querySelector('button').addEventListener('click', () => {
-        const item1 = document.querySelectorAll('li')[1];
-
-        // item1.remove();
-        // 親Node.removeChild(削除するNode)
-        document.querySelector('ul').removeChild(item1);
+        const li =document.createElement('li');
+        const text = document.querySelector('input');
+        li.textContent = text.value;
+        document.querySelector('ul').appendChild(li);
     });
 }
